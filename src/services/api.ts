273afcf822ce56dd2473308baf99d0bbe6ee5d23
Matheus Fischer <matheusfischerr@icloud.com>
2020-08-16 17:44:53 -0300

    i think its done

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,11 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3333',
+  baseURL: 'http://10.0.2.2:3333',
+  responseType: 'json',
+  headers: {
+    'Content-Type': 'application/json',
+  },
 });
 
 export default api;
